d9b33078f48b7366c2aa70423300069b74d19681
Shomirul Hayder Sourov <nm.shomirul@brainstation-23.com>
2024-08-15 19:35:32 +0600

    Drug app is created

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     #local libraries
     'accounts',
+    'drug',
 ]
 
 MIDDLEWARE = [
--- backend/urls.py
+++ backend/urls.py
@@ -5,11 +5,13 @@ from rest_framework.routers import DefaultRouter
 # Add the App library
 #from app import views as app_views
 from accounts import views as accounts_views
+from drug import views as drug_views
 
 router = DefaultRouter()
 # Add the App routes
 #router.register(r'app_name', app_views.AppViewSet)
 router.register(r'accounts', accounts_views.AccountsViewSet, basename='accounts')
+router.register(r'drug', drug_views.DrugViewSet, basename='drug')
 
 urlpatterns = [
     path('api/', include(router.urls)),
