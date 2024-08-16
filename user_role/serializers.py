4736ab6d36f55be4a47e835189d0659d4a0126ca
AlaminIslam-14 <alaminislam.feb@gmail.com>
2024-08-16 18:30:39 +0600

    Set UniqueContraint on user_role/models.py & Added validationon user_role/serializers.py

--- user_role/serializers.py
+++ user_role/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
 from .models import UserRole
-
+ 
 class UserRoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserRole
         fields = '__all__'
+ 
+    def validate(self, data):
+        acc_id = data.get('acc_id')
+        role = data.get('role')
+ 
+        # Updating UserRole
+        if self.instance:
+            if self.instance.role != role:
+                # Check if another role is already assigned to this account
+                if UserRole.objects.filter(acc_id=acc_id).exclude(id=self.instance.id).exists():
+                    raise serializers.ValidationError("This account already has another role assigned.")
+        else:
+            # Creating a new UserRole
+            if UserRole.objects.filter(acc_id=acc_id).exists():
+                raise serializers.ValidationError("This account already has a role assigned.")
+        
+        return data
