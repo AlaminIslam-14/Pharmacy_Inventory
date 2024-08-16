ae931d2165d754815a900b6da78488be9681cd4c
Shomirul Hayder Sourov <nm.shomirul@brainstation-23.com>
2024-08-16 19:09:23 +0600

    Validate Company Id for adding drug

--- drug/models.py
+++ drug/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from accounts.models import Accounts
+from user_role.models import UserRole  
 
 class Drug(models.Model):
     name = models.CharField(max_length=255)
@@ -9,3 +11,9 @@ class Drug(models.Model):
  
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        user_role = UserRole.objects.filter(acc_id=self.acc_id, role=UserRole.SELLER).first()
+        if not user_role:
+            raise ValidationError(f"Account '{self.acc_id}' is not assigned the role 'Company'.")
+        super().save(*args, **kwargs)
--- drug/serializers.py
+++ drug/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Drug
+from user_role.models import UserRole  
 
 class DrugSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drug
         fields = '__all__'
+
+    def validate_acc_id(self, value):
+        if not UserRole.objects.filter(acc_id=value, role=UserRole.SELLER).exists():
+            raise serializers.ValidationError("The account must have the role 'Company' to associate with a drug.")
+        return value
