a83637ab51e37b6ca6cc18dfa715e896ae3da475
Shomirul Hayder Sourov <nm.shomirul@brainstation-23.com>
2024-08-16 22:04:46 +0600

    Change attribute name in orders/models.py: acc_id > seller_id. Change rule of Status

--- orders/serializers.py
+++ orders/serializers.py
@@ -5,4 +5,11 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+        read_only_fields = ['total_cost']
+    
+    def __init__(self, *args, **kwargs):
+        super(OrderSerializer, self).__init__(*args, **kwargs)
+        # Make 'status' read-only for all requests except PUT
+        if self.context['request'].method != 'PUT':
+            self.fields['status'].read_only = True
  
