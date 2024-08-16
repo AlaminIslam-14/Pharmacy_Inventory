9f26c041e9d1f2bd38649f1c6353fb14d4846ddd
AlaminIslam-14 <alaminislam.feb@gmail.com>
2024-08-16 18:24:00 +0600

    license validation checking updated.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,3 +5,11 @@ class AccountsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Accounts
         fields = '__all__'
+        
+    def validate(self, data):
+        license_no = data.get('license_no')
+        
+        if Accounts.objects.filter(license_no=license_no).exists():
+            raise serializers.ValidationError("An account with this license number already exists.")
+        
+        return data
