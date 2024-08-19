eb02b8c6bc01fa3003052cfe325dd4937e5d270b
Mahbub <m.mahbub@brainstation-23.com>
2024-08-19 16:33:31 +0600

    bugfix : update stock upon put request on a particular item

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,15 +1,49 @@
 from rest_framework import serializers
 from .models import Order
+from stock.models import Stock
+from stock.serializers import StockSerializer
+from cart.models import CartItem
+import logging
+
+logger = logging.getLogger(__name__)
 
 class OrderSerializer(serializers.ModelSerializer):
+    updated_stock = serializers.SerializerMethodField()
+
     class Meta:
         model = Order
         fields = '__all__'
-        read_only_fields = ['total_cost']
+        read_only_fields = ['total_cost', 'updated_stock']
     
     def __init__(self, *args, **kwargs):
         super(OrderSerializer, self).__init__(*args, **kwargs)
         # Make 'status' read-only for all requests except PUT
         if self.context['request'].method != 'PUT':
             self.fields['status'].read_only = True
- 
+
+    def get_updated_stock(self, obj):
+        # Get all stock items
+        stocks = Stock.objects.all()
+        # Serialize the stock items
+        return StockSerializer(stocks, many=True).data
+
+    def create(self, validated_data):
+        order = super().create(validated_data)
+        logger.info(f"Order created with ID: {order.id}")
+        return order
+
+    def update_stock(self, order):
+        cart_items = CartItem.objects.filter(cart_id=order.cart_id)
+        for item in cart_items:
+            stock = Stock.objects.get(drug_id=item.drug_id)
+            stock.quantity -= item.quantity
+            stock.save()
+            logger.info(f"Updated stock for drug_id {item.drug_id}: new quantity {stock.quantity}")
+
+    def update(self, instance, validated_data):
+        previous_status = instance.status
+        instance = super().update(instance, validated_data)
+        if previous_status != 'approved' and instance.status == 'approved':
+            logger.info(f"Order {instance.id} status changed to APPROVED. Updating stock.")
+            self.update_stock(instance)
+        return instance
--- stock/serializers.py
+++ stock/serializers.py
@@ -1,22 +1,21 @@
 from rest_framework import serializers
 from .models import Stock
 from django.core.exceptions import ValidationError
- 
+
 class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = '__all__'
- 
+
     def update(self, instance, validated_data):
         drug = validated_data.get('drug_id')
         quantity = validated_data.get('quantity')
- 
+
         existing_stock = Stock.objects.filter(drug_id=drug).first()
- 
+
         quantity = validated_data.get('quantity', instance.quantity)
-        if(quantity < 0 and existing_stock.quantity < -1*quantity):
+        if quantity < 0 and existing_stock.quantity < -1 * quantity:
             raise ValidationError("There is not enough quantity left")
         instance.quantity = existing_stock.quantity + quantity
         instance.save(update_fields=['quantity'])
         return instance
- 
