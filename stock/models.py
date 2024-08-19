2c59c2eab37144a1b5efb30ab312a0fcf8af14a1
AlaminIslam-14 <alaminislam.feb@gmail.com>
2024-08-19 12:07:50 +0600

    Add update method, for drug quanity. uniqueness check for drug and batch id

--- stock/models.py
+++ stock/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from drug.models import Drug
-
+ 
 class Stock(models.Model):
     batch_id = models.CharField(max_length=5)
     drug_id = models.ForeignKey(Drug, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+ 
+    class Meta:
+        unique_together = ('drug_id', 'batch_id')
  
     def __str__(self):
         return f"{self.drug_id.name} - {self.quantity}"
--- stock/serializers.py
+++ stock/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from .models import Stock
-
+ 
 class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = '__all__'
+ 
+    def update(self, instance, validated_data):
+        drug = validated_data.get('drug_id')
+        batch_id = validated_data.get('batch_id')
+        quantity = validated_data.get('quantity')
+ 
+        existing_stock = Stock.objects.filter(drug_id=drug, batch_id=batch_id).first()
+ 
+        quantity = validated_data.get('quantity', instance.quantity)
+        instance.quantity = quantity + existing_stock.quantity
+        instance.save(update_fields=['quantity'])
+        return instance
+ 
