a0496a9a648a4e51efc2835d41748927e2fb6aee
AlaminIslam-14 <alaminislam.feb@gmail.com>
2024-08-20 12:31:10 +0600

    Update Accounts: validation on serializers.py, add filter to views.py. Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     '''
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-
     
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+        ],
 }
